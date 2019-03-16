be7d86ccd170c2aad49bf454e6afae504e25040b
Michael Deneen <mikecdeneen@gmail.com>
2019-03-16 18:03:17 -0400

    * .gitignore: * Program.cs: * GitRepoTest.csproj: * AssemblyInfo.cs: First Commit.
    * GitRepoTest.sln: First commit.

---
+++ GitRepoTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitRepoTest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
