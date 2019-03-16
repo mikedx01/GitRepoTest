bd56439bd584652a0b0a9ff628d632ecb7cdef96
Michael Deneen <mikecdeneen@gmail.com>
2019-03-16 18:20:54 -0400

    changed print text.

--- GitRepoTest/Program.cs
+++ GitRepoTest/Program.cs
@@ -6,7 +6,7 @@ namespace GitRepoTest
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Testing");
         }
     }
 }
